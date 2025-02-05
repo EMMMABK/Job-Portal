dec22f4679b733928ea2b46568be1e0c984e455a
EMMMABK <adilbikiev2@gmail.com>
2025-02-05 19:15:37 +0600

    add Register/Login Page

--- src/main/java/org/example/jobportal/HelloController.java
+++ src/main/java/org/example/jobportal/HelloController.java
@@ -2,13 +2,41 @@ package org.example.jobportal;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Button;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button registerButton;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onLoginButtonClick() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if (username.equals("admin") && password.equals("password")) {
+            welcomeText.setText("Login Successful!");
+        } else {
+            welcomeText.setText("Invalid Credentials!");
+        }
+    }
+
+    @FXML
+    protected void onRegisterButtonClick() {
+        welcomeText.setText("Registration feature coming soon!");
     }
 }
